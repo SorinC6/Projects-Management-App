0d8d7542a0c6cfbdb1cd7365085c0f0c8eb8a33c
Sorin Chis <kish.sorin@yahoo.com>
2019-06-02 16:15:17 +0300

    ADDED SIGNUP COMPONENT AND Route

--- project/src/App.js
+++ project/src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/home/ProjectDetails";
 import Signin from "./components/auth/Signin";
+import Signup from "./components/auth/Signup";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" component={Dashboard} />
         <Route path="/project/:id" component={ProjectDetails} />
         <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
       </Switch>
     </div>
   );
