71707a88d66396f17e746c46063b919de724a097
Sorin Chis <kish.sorin@yahoo.com>
2019-06-02 16:05:38 +0300

    added SIgnin Component and Route

--- project/src/App.js
+++ project/src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/layout/Navbar";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/home/ProjectDetails";
+import Signin from "./components/auth/Signin";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Dashboard} />
         <Route path="/project/:id" component={ProjectDetails} />
+        <Route path="/signin" component={Signin} />
       </Switch>
     </div>
   );
---
+++ project/src/components/auth/Signin.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export class Signin extends Component {
+  state = {};
+
+  handleChange = e => {
+    console.log(e);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Signin;
