c963189bcac68fd654386edd1eedc93700bcad8c
Sorin Chis <kish.sorin@yahoo.com>
2019-06-02 15:09:50 +0300

    added DashBoard and Notification Component

---
+++ project/src/components/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Notification from "./Notification";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m6" />
+        <div className="col s12 m5 offset-m1">
+          <Notification />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ project/src/components/home/ProjectList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="z-depth-0 project-summary">
+          <div className="card-content grey"></div>
+      </div>
+    </div>
+  );
+};
