954b18e7581b1a5af076107e018df682aa5ccd71
Sorin Chis <kish.sorin@yahoo.com>
2019-06-02 14:41:18 +0300

    added NavBar component

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/layout/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>MarioApp</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ project/src/components/layout/SigninLinks.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SigninLinks = () => {
+  return (
+   <ul className="right">
+       <li>
+            <NavLink to="/">New Project</NavLink>
+       </li>
+       <li>
+            <NavLink to="/">Log Out</NavLink>
+       </li>
+       <li>
+            <NavLink to="/"></NavLink>
+       </li>
+   </ul>
+  );
+};
+
+export default SigninLinks;
