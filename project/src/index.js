6e7618463d4ed75f43336d6100e8dc24f681b926
Sorin Chis <kish.sorin@yahoo.com>
2019-06-03 14:35:53 +0300

    added redux and react-redux, create auth and project Reducers

--- project/src/index.js
+++ project/src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+
+const store= createStore()
 
 ReactDOM.render(
   <Router>
