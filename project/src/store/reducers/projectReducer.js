2a997a18797246bec1fcb9a3748a2ed9646e88a1
Sorin Chis <kish.sorin@yahoo.com>
2019-06-03 14:47:07 +0300

    added dummy data in project Reducer

--- project/src/store/reducers/projectReducer.js
+++ project/src/store/reducers/projectReducer.js
@@ -1,4 +1,22 @@
-const initState = {};
+const initState = {
+  project: [
+    {
+      id: "1",
+      title: "Frontend BuildWeek",
+      content: "aloha aloha nansn blah blah blah"
+    },
+    {
+      id: "2",
+      title: "UserInterface BuildWeek",
+      content: "aloha aloha nansn blah blah blah"
+    },
+    {
+      id: "3",
+      title: "Backend BuildWeek",
+      content: "aloha aloha nansn blah blah blah"
+    }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
