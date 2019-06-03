2d45a8a584b6371e0696bf64629a662c64dda3f5
Sorin Chis <kish.sorin@yahoo.com>
2019-06-03 17:15:23 +0300

    create firestore action creator for creating a project

--- project/src/store/actions/projectActions.js
+++ project/src/store/actions/projectActions.js
@@ -1,8 +1,22 @@
-
 export const createProject = project => (
   dispatch,
   getState,
   { getFirebase, getFirestore }
 ) => {
-  dispatch({ type: "CREATE_PROJECT", payload: project });
+  const firestore = getFirestore();
+  firestore
+    .collection("projects")
+    .add({
+      ...project,
+      authorFirstName: "Pacala",
+      authorLastName: "Ion",
+      authorId: 12345,
+      createdAt: new Date()
+    })
+    .then(() => {
+      dispatch({ type: "CREATE_PROJECT", payload: project });
+    })
+    .catch(err => {
+      dispatch({ type: "CREATE_PROJECT_FAIL", payload: err });
+    });
 };
