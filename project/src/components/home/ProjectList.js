46d831969a0002414c888044cb0b2c5655893bd3
Sorin Chis <kish.sorin@yahoo.com>
2019-06-04 19:06:32 +0300

    added dinamic route for project detail

--- project/src/components/home/ProjectList.js
+++ project/src/components/home/ProjectList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
+import { Link } from "react-router-dom";
 
 const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
       {projects &&
         projects.map(pr => {
-          return <ProjectSummary key={pr.id} project={pr} />;
+          return (
+            <Link to={`/project/${pr.id}`}>
+              <ProjectSummary key={pr.id} project={pr} />;
+            </Link>
+          );
         })}
     </div>
   );
