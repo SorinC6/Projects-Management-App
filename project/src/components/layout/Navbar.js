1452c7db903787932671803d0e83dbac85961383
Sorin Chis <kish.sorin@yahoo.com>
2019-06-05 16:14:51 +0300

    conditionaly render  singinin or signout

--- project/src/components/layout/Navbar.js
+++ project/src/components/layout/Navbar.js
@@ -4,22 +4,26 @@ import SigninLinks from "./SigninLinks";
 import SignoutLinks from "./SignoutLinks";
 import { connect } from "react-redux";
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
+  console.log("auth ", auth);
+  const links = auth.uid ? <SigninLinks /> : <SignoutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           MarioPlan
         </Link>
-        <SigninLinks />
-        <SignoutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 const mapStateToProps = state => {
   console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth
+  };
 };
 
 export default connect(mapStateToProps)(Navbar);
