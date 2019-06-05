4e2d1531bcab1391aa79663f6c734a0526717062
Sorin Chis <kish.sorin@yahoo.com>
2019-06-05 14:37:47 +0300

    added Reducer for Login

--- project/src/store/reducers/authReducer.js
+++ project/src/store/reducers/authReducer.js
@@ -1,7 +1,24 @@
-const initState = {};
+const initState = {
+  authError: null
+};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case "LOGIN_ERROR":
+      console.log("ERROR LOginnnnnn");
+      return {
+        ...state,
+        authError: "LOGIN ERROR"
+      };
+    case "LOGIN_SUCCESS":
+      console.log("Login Success");
+      return {
+        ...state,
+        authError: null
+      };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
