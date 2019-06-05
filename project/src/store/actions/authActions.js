7546955b7b75100b765b0c7628de657a5fdaa2c9
Sorin Chis <kish.sorin@yahoo.com>
2019-06-05 15:10:23 +0300

    added action creator and reducer for logout firebase

--- project/src/store/actions/authActions.js
+++ project/src/store/actions/authActions.js
@@ -11,3 +11,15 @@ export const signIn = creds => (dispatch, getState, { getFirebase }) => {
       dispatch({ type: "LOGIN_ERROR", payload: err });
     });
 };
+
+export const signOut = () => (dispatch, getState, { getFirebase }) => {
+  const firebase = getFirebase();
+
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      dispatch({ type: "SIGNOUT_SUCCESS" });
+    })
+    .catch();
+};
