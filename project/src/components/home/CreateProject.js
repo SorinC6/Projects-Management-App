da58acc40a225bd0f6da2ccd61e13feec65ae441
Sorin Chis <kish.sorin@yahoo.com>
2019-06-06 15:25:57 +0300

    added timestamp with momentJs library

--- project/src/components/home/CreateProject.js
+++ project/src/components/home/CreateProject.js
@@ -19,7 +19,7 @@ export class CreateProject extends Component {
     e.preventDefault();
     //console.log(this.state);
     this.props.createProject(this.state);
-    this.props.history.push('/')
+    this.props.history.push("/");
   };
   render() {
     const { auth } = this.props;
