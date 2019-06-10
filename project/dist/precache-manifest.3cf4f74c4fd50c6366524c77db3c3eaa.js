a7c93bf076ce4386da11baeaa158cd85bef64493
Sorin Chis <kish.sorin@yahoo.com>
2019-06-10 16:52:34 +0300

    added firebase deployment

---
+++ project/dist/precache-manifest.3cf4f74c4fd50c6366524c77db3c3eaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7538783fe6fbed39ea3a5cb9503ea1ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fbe9fbce4b3cc2a2c72",
+    "url": "/static/css/main.c388d05d.chunk.css"
+  },
+  {
+    "revision": "faac2ee12cee53d8f8a7",
+    "url": "/static/js/2.885e0082.chunk.js"
+  },
+  {
+    "revision": "7fbe9fbce4b3cc2a2c72",
+    "url": "/static/js/main.9b24da08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
