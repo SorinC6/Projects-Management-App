721d24204014463a15888a00692a3275ab35cf21
Sorin Chis <kish.sorin@yahoo.com>
2019-06-10 16:31:34 +0300

    added Notificatation Component  content

--- project/src/components/dashboard/Notification.js
+++ project/src/components/dashboard/Notification.js
@@ -1,17 +1,28 @@
 import React from "react";
+import moment from "moment";
 
 const Notification = props => {
   const { notification } = props;
+  console.log(notification);
   return (
     <div className="section">
       <div className="card z-depth-0">
         <div className="card-content">
           <span className="card-title">Notification</span>
           <ul className="notification">
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
+            {notification &&
+              notification.map(item => {
+                //debugger;
+                return (
+                  <li key={item.id}>
+                    <span className="pink-text">{item.user}</span>
+                    <span>{item.content}</span>
+                    <div className="grey-text note-date">
+                      {moment(item.time.toDate()).fromNow()}
+                    </div>
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </div>
