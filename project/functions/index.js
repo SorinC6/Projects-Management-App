eb76acae5d3ca2060b64a4a5b87998f82986553b
Sorin Chis <kish.sorin@yahoo.com>
2019-06-10 15:07:48 +0300

    added firebase function for notification when a user its created

--- project/functions/index.js
+++ project/functions/index.js
@@ -6,25 +6,44 @@ admin.initializeApp(functions.config().firebase);
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Sorin!");
+  response.send("Hello from Sorin!");
 });
 
-const createNotification = (notification => {
-    return admin.firestore()
-        .collection("notifications")
-        .add(notification)
-        .then(doc => console.log('notification added ', doc)); //36 ----
-});
+const createNotification = notification => {
+  return admin
+    .firestore()
+    .collection("notifications")
+    .add(notification)
+    .then(doc => console.log("notification added ", doc)); //36 ----
+};
 
 exports.projectCreated = functions.firestore
-    .document("projects/{projectId}")
-    .onCreate(doc => {
-        const project = doc.data();
-        const notification = {
-            content: "Added a new Project",
-            user: `${project.authorFirstname} ${project.authorLastname}`,
-            time: admin.firestore.FieldValue.serverTimestamp()
-        };
+  .document("projects/{projectId}")
+  .onCreate(doc => {
+    const project = doc.data();
+    const notification = {
+      content: "Added a new Project",
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
 
-        return createNotification(notification)
+    return createNotification(notification);
+  });
+
+exports.userJoined = functions.auth.user().onCreate(user => {
+  return admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .get()
+    .then(doc => {
+      const newUser = doc.data();
+      const notification = {
+        content: "Joined the party",
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      };
+
+      return createNotification(notification);
     });
+});
