57ce715c124fc557d2a9ae2a0e397b3f7144b84c
Sorin Chis <kish.sorin@yahoo.com>
2019-07-11 11:12:05 +0300

    change title

--- project/src/components/layout/Navbar.js
+++ project/src/components/layout/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = props => {
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          MarioPlan
+          ProjectPlan
         </Link>
         {links}
       </div>
